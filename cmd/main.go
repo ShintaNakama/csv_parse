00c835ae6cbacd80dfa46a6a03b719bd8292cc66
ShintaNakama <shintanakama77@gmail.com>
2020-05-24 21:41:32 +0900

    feat: first commit

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ShintaNakama/csv_parse/internal/coconala"
+)
+
+func passArgs() string {
+	flag.Parse()
+	args := strings.Join(flag.Args(), "")
+
+	return args
+}
+
+func main() {
+	args := passArgs()
+
+	file, err := os.Open(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	datas, err := coconala.Output(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	for _, data := range datas {
+		fmt.Fprintln(os.Stdout, data.SalesDate)
+		fmt.Fprintln(os.Stdout, data.SalesAmount)
+	}
+}
---
+++ internal/coconala/coconala.go
@@ -0,0 +1,11 @@
+package coconala
+
+type SalesData struct {
+	SalesDate      string `csv:"売上確定日"`
+	TolkRoomNo     string `csv:"トークルームNo"`
+	ServiceName    string `csv:"サービス名/見積り・仕事の相談名"`
+	BuyerID        string `csv:"購入者ID"`
+	BuyerName      string `csv:"購入者名"`
+	SalesBreakdown string `csv:"内訳"`
+	SalesAmount    string `csv:"売上金額"`
+}
