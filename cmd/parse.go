092d987e726f6afc63347450127b6beea23320c6
ShintaNakama <shintanakama77@gmail.com>
2020-05-27 00:42:14 +0900

    fix: main.go

---
+++ cmd/parse.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ShintaNakama/csv_parse/internal/coconala"
+)
+
+//func passArgs() string {
+//	flag.Parse()
+//	args := strings.Join(flag.Args(), "")
+//
+//	return args
+//}
+
+func parse(args string) {
+	csv := args
+
+	file, err := os.Open(csv)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	datas, err := coconala.Output(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	for _, data := range datas {
+		fmt.Println(data.SalesDate)
+		fmt.Println(data.SalesAmount)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ShintaNakama/csv_parse
 
 go 1.14
 
-require github.com/gocarina/gocsv v0.0.0-20200330101823-46266ca37bd3
+require (
+	github.com/gocarina/gocsv v0.0.0-20200330101823-46266ca37bd3
+	github.com/spf13/cobra v1.0.0
+)
