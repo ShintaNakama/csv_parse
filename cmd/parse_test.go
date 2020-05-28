37e6b06e7d0bba863cc4a8397d2e763c75a04b41
ShintaNakama <shintanakama77@gmail.com>
2020-05-29 00:56:08 +0900

    refactor: solve golint

--- cmd/parse_test.go
+++ cmd/parse_test.go
@@ -15,27 +15,6 @@ func createExpectedData(path string) ([]byte, error) {
 	}
 
 	return expectedData, nil
-
-	//file, err := os.Open(path)
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	//defer file.Close()
-	//reader := bufio.NewReader(file)
-	//var expectData []string
-	//for {
-	//	line, _, err := reader.ReadLine()
-	//	if err == io.EOF {
-	//		break
-	//	}
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	expectData = append(expectData, string(line))
-	//}
-
-	//return expectData, nil
 }
 
 func TestParse(t *testing.T) {
@@ -49,16 +28,7 @@ func TestParse(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if bytes.Compare(expected, stdout.Bytes()) != 0 {
+	if !bytes.Equal(expected, stdout.Bytes()) {
 		t.Fatal("not matched")
 	}
-	//scanner := bufio.NewScanner(stdout)
-	//var i int
-	//for scanner.Scan() {
-	//	line := scanner.Text()
-	//	if line != expected[i] {
-	//		t.Errorf("not matched expected: %s result: %s",expected[i], line)
-	//	}
-	//  i++
-	//}
 }
